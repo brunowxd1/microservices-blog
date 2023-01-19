a72eadb54a52a87251f54eed8e0cc73946f77fba
Bruno Jagmin <brunojagminferreira@gmail.com>
2023-01-19 02:12:32 -0300

    first commit

---
+++ comments/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const commentsByPostId = {};
+
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post("/posts/:id/comments", (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+
+  const { content } = req.body;
+  const { id: postId } = req.params;
+
+  const comments = commentsByPostId[postId] || [];
+
+  comments.push({ id: commentId, content });
+
+  commentsByPostId[postId] = comments;
+
+  res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+  console.log("Comments listening on 4001");
+});
